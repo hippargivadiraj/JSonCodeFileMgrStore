15761c3e6d7e2afc6c519b17ca136fdb590ede6a
Vadiraj Hippargi <Vadi@leadconsultant.com>
2022-07-28 20:18:35 -0700

    Initial Commit

---
+++ JSonCodeFileMgrStore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JSonCodeFileMgrStore
+//
+//  Created by Vadiraj Hippargi on 7/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JSonCodeFileMgrStore/JSonCodeFileMgrStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSonCodeFileMgrStoreApp.swift
+//  JSonCodeFileMgrStore
+//
+//  Created by Vadiraj Hippargi on 7/28/22.
+//
+
+import SwiftUI
+
+@main
+struct JSonCodeFileMgrStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
